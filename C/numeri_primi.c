e716ef6fead12cf2295ff7f99eeb40d45a460e2b
Antonio <35410603+redyz13@users.noreply.github.com>
2021-10-22 12:36:37 +0200

    new file:   numeri_primi.c

---
+++ C/numeri_primi.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int primo(int x);
+
+int main() {
+	int i = 1;
+	
+	printf("[Lista numeri primi]\n\n");
+
+	while(i < 100) {
+		if(primo(i))
+			printf("%d\n", i);
+		i++;
+	}
+
+	return 0;
+}
+
+int primo(int x) {
+	int i = 2, flag = 1;
+
+	while(i < x) {
+		flag = flag && x % i != 0;
+		i++;
+	}
+
+	if(flag)
+		return flag;
+	else
+		return 0;
+}
