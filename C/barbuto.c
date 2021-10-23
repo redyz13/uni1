29f1739691d57ee0dc9d427cb68c81ad218df9bd
Antonio <35410603+redyz13@users.noreply.github.com>
2021-10-23 12:50:59 +0200

    modified:   barbuto.c

--- C/barbuto.c
+++ C/barbuto.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
-
 int main() {
-	int i = 0;
+    int i = 0;
 
-	while(i <= 10) {
-		printf("%d è %spari\n", i, i % 2 == 0 ? "" : "dis");
+    while(i <= 10) {
+        printf("%d è %spari\n", i, i % 2 == 0 ? "" : "dis");
 
-		i++;
-	}
+        i++;
+    }
 
-	printf("\n");
-	
-	return 0;
+    printf("\n");
+
+    return 0;
 }
