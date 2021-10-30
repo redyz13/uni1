8bb34cb6924390edd779a4981e10902107315d5a
Antonio <35410603+redyz13@users.noreply.github.com>
2021-10-30 19:01:49 +0200

    new file:   stampa_triangoli.c

---
+++ C/10_21/stampa_triangoli.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, ws, star = 1;
+
+    printf("Inserire il numero di colonne: ");
+    scanf("%d", &n);
+
+    ws = n - 1;
+
+    while(n > 0) {
+        if(n > 1) {
+            for(int i = 0; i < ws; i++)
+                printf(" ");
+
+            for(int i = 0; i < star; i++) {
+                printf("*");
+            }
+        }
+        else {
+            for(int i = 0; i < star; i++)
+                printf("*");
+        }
+
+        star += 2;
+        ws--;
+        n--;
+        printf("\n");
+    }
+}
