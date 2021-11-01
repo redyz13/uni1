2f1cc080185caf41da330acb654f79a10ed09948
Antonio <35410603+redyz13@users.noreply.github.com>
2021-11-01 18:10:06 +0100

    modified:   stampa_triangoli.c

--- C/10_21/stampa_triangoli.c
+++ C/10_21/stampa_triangoli.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int n, star = 1;
+    int n, i, star = 1;
 
     printf("Inserire il numero di righe: ");
     scanf("%d", &n);
 
     while(n > 0) {
-        for(int i = 0; i < n - 1; i++)
+        for(i = 0; i < n - 1; i++)
             printf(" ");
 
-        for(int i = 0; i < star; i++) {
+        for(i = 0; i < star; i++) {
             printf("*");
         }
 
