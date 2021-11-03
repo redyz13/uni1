0196c3716b3853b0556d0060d56c7a82362a6b57
Antonio <35410603+redyz13@users.noreply.github.com>
2021-11-03 21:35:34 +0100

    overall more barbuto

--- C/10_21/barbuto.c
+++ C/10_21/barbuto.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int i = 0;
+    int i;
 
-    while(i <= 10) {
+    for(i = 1; i <= 10; i++)
         printf("%d è %spari\n", i, i % 2 == 0 ? "" : "dis");
-
-        i++;
-    }
 
     printf("\n");
 
