32f29af7b4f778c50b2cf3326f6a275cb2ae6664
Antonio <35410603+redyz13@users.noreply.github.com>
2022-03-13 22:46:13 +0100

    new file:   bubble_sort_args.c

---
+++ PSD/03_22/esercizi_10_03/bubble_sort_args.c
@@ -0,0 +1,78 @@
+/* Scrivere un programma che riceve da riga di comando 
+ * una successione di numeri e ne effettui l’ordinamento 
+ * attraverso una funzione void ordina(int*) con uno degli 
+ * algoritmi di ordinamento implementati al corso. 
+ * Il risultato va mostrato su standard output
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define getchar() getc(stdin)
+
+// Bubble sort
+void ordina(int *a, int lenght);
+int *crea_array(int argc, char **argv);
+
+int main(int argc, char **argv) {
+    int *array, i;
+
+    if(argc < 2) {
+        printf("Utilizzo: <%s numero1 numero2 ...>\n", argv[0]);
+    }
+    else {
+        array = crea_array(argc, argv);
+
+        printf("Numeri inseriti:\n");
+
+        for(i = 0; i < argc - 1; i++) {
+            printf("%d ", array[i]);
+        }
+
+        printf("\n\nNumeri ordinati:\n");
+
+        ordina(array, argc - 1);
+
+        for(i = 0; i < argc - 1; i++) {
+            printf("%d ", array[i]);
+        }
+
+        putchar('\n');
+    }
+
+    return 0;
+}
+
+int *crea_array(int argc, char **argv) {
+    int *array;
+    int i;
+
+    array = malloc(sizeof(int) * (argc - 1));
+
+    if(!array) {
+        return NULL;
+    }
+
+    for(i = 0; i < argc - 1; i++) {
+        array[i] = atoi(argv[i+1]);
+    }
+
+    return array;
+}
+
+// Bubble sort
+void ordina(int *a, int lenght) {
+    int i, j;
+    int tmp;
+
+    for(i = 0; i < lenght; i++) {
+        for(j = 0; (j < lenght - 1 - i); j++) {
+            if(a[j] > a[j+1]) {
+                tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+            }
+        }
+    }
+}
---
+++ PSD/03_22/esercizi_10_03/merge_args.c
@@ -0,0 +1,65 @@
+/* Scrivere un programma che implementa la funzione char *merge(char*, char*) 
+ * che prende da riga di comando due stringhe e produce una nuova stringa
+ * che è il risultato della concatenazione delle due date in input. 
+ * Il risultato va mostrato su standard output
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define getchar() getc(stdin)
+
+char *leggi_stringa(int buff); 
+void copia_stringa(char *s1, const char *s2);
+char *merge(const char *s1, const char *s2);
+
+int main(int argc, char **argv) {
+    char *concat;
+
+    if(argc < 3) {
+        printf("Utilizzo: <%s nome cognome>\n", argv[0]);
+    }
+    else {
+        printf("Nome inserito: %s", argv[1]);
+        printf("\nCognome inserito: %s", argv[2]);
+
+        concat = merge(argv[1], argv[2]);
+
+        printf("\nConcatenazione di nome e cognome: %s", concat);
+    }
+
+    return 0;
+}
+
+void copia_stringa(char *s1, const char *s2) {
+    char *pi = s1;
+
+    while(*s2) {
+        *pi++ = *s2++;
+    }
+
+    *pi = '\0';
+}
+
+char *merge(const char *s1, const char *s2) {
+    char *p;
+    char *pi;
+
+    p = malloc(strlen(s1) + strlen(s2) + 1);
+
+    if(!p) {
+        return NULL;
+    }
+
+    pi = p;
+
+    copia_stringa(pi, s1);
+
+    while(*pi)
+        pi++;
+
+    copia_stringa(pi, s2);
+
+    return p;
+}
