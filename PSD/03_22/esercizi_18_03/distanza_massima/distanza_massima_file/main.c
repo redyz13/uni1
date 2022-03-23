2f95f9b7da2d459c3f2f7a1a2afb97038bbeaee3
Antonio <35410603+redyz13@users.noreply.github.com>
2022-03-23 17:43:37 +0100

    new file:   distanza_massima/distanza_massima_file/main.c

--- PSD/03_22/esercizi_18_03/distanza_massima/distanza_massima_file/main.c
+++ PSD/03_22/esercizi_18_03/distanza_massima/distanza_massima_file/main.c
@@ -2,39 +2,39 @@
 #include <stdlib.h>
 #include "punto.h"
 
-int main(int argc, char *argv[]) {
-    int i, j, k;
+#define NPUNTI 30
+
+int main(void) {
+    int i, k;
     int n = 0, m = 0;
     float d = 0;
     float ascissa = 0, ordinata = 0, tmp;
     punto *p;
+    FILE *fp;
 
-    // Carica tutto l'input: coppie e distanza
-    if(argc < 4 || (argc % 2 != 0)) {
-        fprintf(stderr, "formato: <%s, distanza, coppie...>\n", argv[0]);
+    fp = fopen("input.txt", "r");
+
+    if(!fp) {
+        fprintf(stderr, "File non trovato...\n");
         return -1;
     }
 
-    n = (argc - 2) / 2;
-
-    p = malloc(sizeof(punto) * n);
+    p = malloc(sizeof(punto) * NPUNTI);
 
     if(!p) return -1;
 
-    d = atof(argv[1]);
+    // Lettura della distanza
+    fscanf(fp, "%f", &d);
 
-    j = 0;
-
-    for(i = 2; i < argc; i += 2) {
-        ascissa = atof(argv[i]);
-        ordinata = atof(argv[i+1]);
-
-        p[j] = creapunto(ascissa, ordinata);
-
-        stampa(p[j]);
-
-        j++;
+    while(fscanf(fp, "%f%f", &ascissa, &ordinata) == 2) {
+        p[n++] = creapunto(ascissa, ordinata);
     }
+
+    fclose(fp);
+
+    p = realloc(p, sizeof(punto) * n);
+
+    if(!p) return -1;
 
     // Calcola le distanze a coppie e controlla che siano minori di d
     for(i = 0; i < n; i++) {
