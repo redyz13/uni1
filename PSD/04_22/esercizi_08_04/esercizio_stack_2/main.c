00a87d31ac6d4bcd9fa20c5a019e3a35166126f1
Antonio <35410603+redyz13@users.noreply.github.com>
2022-04-22 09:24:12 +0200

    added esercizio_stack_2

---
+++ PSD/04_22/esercizi_08_04/esercizio_stack_2/main.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include "stack.h"
+
+void inserisci(Stack s);
+Stack clone(Stack s); 
+Stack inverti(Stack s);
+Stack merge(Stack s1, Stack s2);
+Stack removeItems(Stack s, Item it);
+
+int main(void) {
+    Stack s, rev, mer; 
+
+    s = newStack();
+    
+    inserisci(s);
+
+    putchar('\n');
+
+    printStack(s);
+
+    putchar('\n');
+
+    rev = inverti(s);   
+
+    printStack(rev);
+
+    mer = merge(s, rev);   
+
+    putchar('\n');
+
+    printStack(mer);
+
+    putchar('\n');
+
+    mer = removeItems(mer, 5);
+
+    printStack(mer);
+
+    freeStack(s);
+    freeStack(rev);
+    freeStack(mer);
+    
+    return 0;
+}
+
+void inserisci(Stack s) {
+    int x; 
+
+    for(;;) {
+        printf("Inserisci zoccola: ");
+        scanf("%d", &x);
+
+        if(x == -1) break;
+        
+        push(s, x);
+    }
+}
+
+Stack merge(Stack s1, Stack s2) {
+    Stack mer, tmp1, tmp2;
+
+    mer = newStack();
+    
+    tmp1 = clone(s1);
+    tmp2 = clone(s2);
+
+    while(!isEmpty(tmp1)) {
+        push(mer, top(tmp1));
+        pop(tmp1);
+    }
+
+    while(!isEmpty(tmp2)) {
+        push(mer, top(tmp2));
+        pop(tmp2);
+    }
+
+    freeStack(tmp1);
+    freeStack(tmp2);
+
+    return mer;
+}
+
+Stack clone(Stack s) {
+    Stack clone, tmp; 
+    
+    tmp = newStack();
+    clone = newStack();
+
+    while(!isEmpty(s)){
+        push(tmp, top(s));
+        pop(s);
+    }
+
+    while(!isEmpty(tmp)) {
+        push(clone, top(tmp));
+        push(s, top(tmp));
+        pop(tmp);
+    }
+
+    freeStack(tmp);
+    
+    return clone;
+} 
+
+Stack inverti(Stack s) {
+    Stack rev, tmp;
+
+    rev = newStack();
+    tmp = clone(s);
+
+    while(!isEmpty(tmp)) {
+        push(rev, top(tmp));
+        pop(tmp);
+    }
+
+    freeStack(tmp);
+
+    return rev;
+}
+
+Stack removeItems(Stack s, Item it) {
+    Stack new;
+
+    new = newStack();
+
+    while(!isEmpty(s)) {
+        if(!isEqual(it, top(s))) {
+            push(new, top(s));
+        }
+        
+        pop(s);
+    }
+
+    freeStack(s);
+
+    Stack t = new;
+
+    new = inverti(new);
+
+    freeStack(t);
+
+    return new;
+}
---
+++ PSD/04_22/esercizi_08_04/esercizio_stack_2/stack.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+struct Stack {
+    struct Node* head;
+    int size;
+};
+
+struct Node {
+    struct Node* next;
+    Item data;
+};
+
+Stack newStack(void) {
+    Stack s;
+
+    s = malloc(sizeof(struct Stack));
+    if(s == NULL) return NULL;
+
+    s->head = NULL;
+    s->size = 0;
+
+    return s;
+}
+
+int isEmpty(Stack s) {
+    return (s->size) == 0;
+}
+
+int push(Stack s, Item it) {
+    struct Node* new;
+    
+    new = malloc(sizeof(struct Node));
+    if(new == NULL) return 0;
+    
+    new->data = it;
+    
+    new->next = s->head; 
+    s->head = new;
+
+    (s->size)++;
+   
+    return 1; 
+}
+
+int pop(Stack s) {
+    struct Node* tmp;
+
+    if(isEmpty(s)) return 0;
+
+    tmp = s->head;
+    s->head = s->head->next;
+    free(tmp);
+    
+    (s->size)--;
+
+    return 1;
+}
+
+Item top(Stack s){
+    if(isEmpty(s)) return NULLITEM;
+
+    return s->head->data;
+}
+
+void printStack(Stack s) {
+    struct Node* head = s->head;
+    int i = 0;
+
+    while(head != NULL) {
+        printf("Element %d = ", i);
+        printItem(head->data);
+        putchar('\n');
+        head = head->next;
+        i++;
+    }
+}
+
+Stack reverseStack(Stack s) {
+    struct Node *head = s->head;
+    Stack reverse;
+    Item val;
+    
+    reverse = newStack();
+
+    while(head != NULL){
+        val = head->data;
+        push(reverse, val);
+        head = head->next;
+    }
+
+    return reverse;
+}
+
+
+/*Stack merge(Stack s1, Stack s2) {
+    struct Node *head_1 = s1->head;
+    struct Node *head_2 = s2->head;
+    Item val;
+
+    Stack new;
+
+    new = newStack();
+
+    while(head_2 != NULL) {
+        val = head_2->data;
+        push(new, val);
+        head_2 = head_2->next;
+    }
+
+    while(head_1 != NULL) {
+        val = head_1->data;
+        push(new, val);
+        head_1 = head_1->next;
+    }
+
+    return new;
+}
+
+Stack deleteItems(Stack s, Item it) {
+    struct Node *tmp, *new_head;
+    struct Node *previous;
+
+    // If the item is at the beginning change the references
+    if(isEqual(it, s->head->data)) {
+        tmp = s->head;
+        s->head = s->head->next;
+        free(tmp);
+    }
+
+    new_head = s->head;
+    previous = s->head;
+
+    while(s->head != NULL) {
+        if(isEqual(it, s->head->data)) {
+            tmp = s->head;
+            s->head = s->head->next;
+            previous->next = s->head;
+            free(tmp);
+        }
+        else {
+            previous = s->head;
+            s->head = s->head->next;
+        }
+    }
+
+    s->head = new_head;
+
+    return s;
+}*/
+
+void freeStack(Stack s) {
+    struct Node* tmp;
+
+    while(s->head != NULL) {
+        tmp = s->head;
+        s->head = s->head->next;
+        free(tmp);
+    }
+
+    free(s);
+}
