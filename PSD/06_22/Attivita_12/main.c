8b02ebf7f1247db6b88e73179227d94f4996721e
Antonio <35410603+redyz13@users.noreply.github.com>
2022-06-11 19:23:02 +0200

    modified:   main.c 	modified:   util.h

--- PSD/06_22/Attivita_12/main.c
+++ PSD/06_22/Attivita_12/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "util.h"
 
 int main(void) {
@@ -8,6 +9,27 @@ int main(void) {
 
     printf("Coda: ");
     printPQueue(q);
+
+    printf("\n\nMassimo: %d", maxPQueue(q));
+    printf("\nMinimo: %d", minPQueue(q));
+
+    printf("\n\nCoda dopo min: ");
+    printPQueue(q);
+
+    incrementaChiave(q, 6, 13);
+
+    printf("\nCoda dopo incrementa chiave: ");
+    printPQueue(q);
+
+    int arr[] = { 5, 7, 9, 3, 11, 29, 4 };
+
+    printf("\n\nArray non ordinato: ");
+    stampaArray(arr, 7);
+
+    heapify(arr, 7);
+
+    printf("\nArray dopo heapify: ");
+    stampaArray(arr, 7);
 
     putchar('\n');
 
@@ -19,14 +41,93 @@ PQueue creaPQueue(void) {
 
     insert(q, 15);
     insert(q, 12);
-    insert(q, 14);
     insert(q, 10);
     insert(q, 7);
     insert(q, 6);
     insert(q, 9);
     insert(q, 2);
-    insert(q, 5);
     insert(q, 3);
 
     return q;
+}
+
+Key maxPQueue(PQueue q) {
+    return getMax(q);
+}
+
+Key minPQueue(PQueue q) {
+    PQueue tmp = newPQ();
+    int min, k;
+
+    min = getMax(q);
+    deleteMax(q);
+    insert(tmp, min);
+
+    while (!isEmpty(q)) {
+        k = getMax(q);
+        insert(tmp, k);
+
+        if (min > k) {
+            min = k;
+        }
+
+        deleteMax(q);
+    }
+
+    while (!isEmpty(tmp)) {
+        k = getMax(tmp);
+        insert(q, k);
+        deleteMax(tmp);
+    }
+
+    free(tmp);
+
+    return min;
+}
+
+void incrementaChiave(PQueue q, Key k, Key newK) {
+    if (k >= newK) return;
+
+    PQueue tmp = newPQ();
+    int key;
+
+    while (!isEmpty(q)) {
+        key = getMax(q);
+        insert(tmp, key);
+        deleteMax(q);
+    }
+
+    while (!isEmpty(tmp)) {
+        key = getMax(tmp);
+
+        if (key == k) {
+            insert(q, newK);
+        }
+        else {
+            insert(q, key);
+        }
+
+        deleteMax(tmp);
+    }
+
+    free(tmp);
+}
+
+void stampaArray(int *arr, int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+}
+
+void heapify(int *arr, int size) {
+    int i, j, tmp;
+
+    for (i = 0; i < size - 1; i++)  {
+        for (j = 0; j < size - i - 1; j++) {
+            if (arr[j] < arr[j+1]) {
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
 }
--- PSD/06_22/Attivita_12/util.h
+++ PSD/06_22/Attivita_12/util.h
@@ -1,3 +1,13 @@
 #include "pqueue.h"
 
 PQueue creaPQueue(void);
+
+Key maxPQueue(PQueue q);
+
+Key minPQueue(PQueue q);
+
+void incrementaChiave(PQueue q, Key k, Key newK);
+
+void stampaArray(int *arr, int size);
+
+void heapify(int *arr, int size);
